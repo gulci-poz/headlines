8370f28c79cf5a72848bd68db1531a7f506c5c30
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2016-08-05 23:44:25 +0200

    2016-08-05 jinja templates, time for get requests

--- headlines.py
+++ headlines.py
@@ -1,12 +1,16 @@
-from flask import Flask
+import os
 import feedparser
+from flask import Flask
+from flask import send_from_directory
+from flask import render_template
 
 app = Flask(__name__)
 
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn': 'http://rss.cnn.com/rss/edition.rss',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
-             'iol': 'http://classic.iol.co.za/cmlink/1.640'
+             'iol': 'http://classic.iol.co.za/cmlink/1.640',
+             'epoznan': 'http://epoznan.pl/rss.php',
              }
 
 
@@ -21,23 +25,36 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 @app.route('/<publication>')
 def get_news(publication='bbc'):
     feed = feedparser.parse(RSS_FEEDS[publication])
-    first_article = feed['entries'][0]
-    return """
-        <html>
-            <body>
-                <h1>BBC Headlines</h1>
-                <b>{0}</b>
-                <br />
-                <i>{1}</i>
-                <br />
-                <p>{2}</p>
-                <br />
-            </body>
-        </html>
-    """.format(first_article.get('title'),
-               first_article.get('published'),
-               first_article.get('summary'))
+    # możemy przekazać do szablonu całą listę słowników z feedami
+    # odrobinę logiki (iterowanie po elementach) przerzucamy do szablonu
+    # do klucza słownika w szablonie odwołujemy się za pomocą kropki
+    return render_template('home.html', articles=feed['entries'])
 
+    # first_article = feed['entries'][0]
+    # zamiast first_article['title'] używamy get()
+    # get() to metoda słownika, w razie braku klucza nie dostaniemy błędu
+    # return """
+    #     <html>
+    #         <body>
+    #             <h1>BBC Headlines</h1>
+    #             <b>{0}</b>
+    #             <br />
+    #             <i>{1}</i>
+    #             <br />
+    #             <p>{2}</p>
+    #             <br />
+    #         </body>
+    #     </html>
+    # """.format(first_article.get('title'),
+    #            first_article.get('published'),
+    #            first_article.get('summary'))
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico',
+                               mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
