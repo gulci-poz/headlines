29d9b924613540176bdc67e60de4c32a8d4a4538
gulci <sebastian.gulczynski.poznan@gmail.com>
2016-08-09 14:37:56 +0200

    2016-08-09 deployment configuration for mydevil and local apache on ubuntu server

--- headlines.py
+++ headlines.py
@@ -1,9 +1,12 @@
-import os
+import json
 import feedparser
 from flask import Flask
 from flask import send_from_directory
 from flask import render_template
 from flask import request
+import os
+from urllib.parse import quote
+from urllib.request import urlopen
 
 app = Flask(__name__)
 
@@ -12,6 +15,10 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
              'iol': 'http://classic.iol.co.za/cmlink/1.640',
              'epoznan': 'http://epoznan.pl/rss.php',
+             }
+
+DEAFAULTS = {'publication': 'bbc',
+             'city': 'Poznan,PL'
              }
 
 
@@ -27,7 +34,8 @@ def get_news():
         publication = query.lower()
 
     feed = feedparser.parse(RSS_FEEDS[publication])
-    return render_template('home.html', articles=feed['entries'])
+    weather = get_weather('Poznan,PL')
+    return render_template('home.html', articles=feed['entries'], weather=weather)
 
 
 @app.route('/favicon.ico')
@@ -35,6 +43,30 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico',
                                mimetype='image/vnd.microsoft.icon')
+
+
+def get_weather(query):
+    # lokalizację będziemy mieli w {}
+    # np. Poznan,PL
+    api_url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=04eae95ad58ff3d6b45089b57a134b32'
+    # lokalizację odzieramy ze spacji i przecinka
+    query = quote(query)
+    # formatujemy url, podstawiając przygotowaną lokalizację
+    url = api_url.format(query)
+    # ściągamy dane z przygotowanego urla, dostaniemy obiekt HTTPResponse w postaci bajtów
+    data = urlopen(url).read().decode('utf-8')
+    # parsujemy dane do json
+    parsed = json.loads(data)
+    # musimy zainicjować zmienną tutaj, jeśli zrobimy to w if, to zmienna będzie lokalna dla segmentu if
+    weather = None
+
+    if parsed.get('weather'):
+        weather = {'description': parsed['weather'][0]['description'],
+                   'temperature': parsed['main']['temp'],
+                   'city': parsed['name']}
+
+    return weather
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
---
+++ passenger_wsgi.py
@@ -0,0 +1,3 @@
+import sys, os
+sys.path.append(os.getcwd())
+from headlines import app as application
